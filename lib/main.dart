eabcb2744380cde65bce676fde6fe12ebdbf99cb
Berkaysapmaz <berkay30082000@hotmail.com>
2023-10-30 10:54:15 +0300

    push notifications added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_app/api/firebase_api.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async{ 
-
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp();
+await FirebaseApi().initNotifications();
   runApp(MyApp());
 }
 
